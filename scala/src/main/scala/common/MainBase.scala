facc49699d5bbf0005fd89438afff2832d1e24a5
Mihai Stanescu <mihai.stanescu@tundra.com>
2022-12-12 07:44:21 +0100

    Print updates

--- scala/src/main/scala/common/MainBase.scala
+++ scala/src/main/scala/common/MainBase.scala
@@ -4,18 +4,20 @@ import scala.io.Source
 import scala.util.Try
 
 abstract class MainBase(day: Int) {
+
   def main(args: Array[String]): Unit = {
     Try(Source.fromResource(s"day-$day-test.txt").getLines().toList).toOption.foreach { smallInput =>
       println(s"test result = ${run(smallInput)}")
     }
 
     Try(Source.fromResource(s"day-$day-small.txt").getLines().toList).toOption.foreach { smallInput =>
-      println(s"small result = ${run(smallInput)}")
+      println(s"${getClass.getSimpleName} small result = ${run(smallInput)}")
     }
 
     val inputFile = Source.fromResource(s"day-$day.txt").getLines().toList
-    println(s"result = ${run(inputFile)}")
+    println(s"${getClass.getSimpleName} big   result = ${run(inputFile)}")
   }
+
 
   def run(inputFile: List[String]): String
 }
